8018d199868da9be0b3ba9d135684aed4fd78ea4
AnotherJelly <bespalov-maksim@list.ru>
2025-05-26 18:51:59 +0300

    Upload project files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,73 @@
-import Image from "next/image";
+"use client";
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import Cookies from "js-cookie";
 
-export default function Home() {
+// Тип данных урока
+interface Lesson {
+  id: number;
+  title: string;
+  description: string;
+  progress: number;
+}
+
+const lessonsData: Omit<Lesson, 'progress'>[] = [
+  { id: 1, title: 'Что такое SEO?', description: 'Что такое SEO, почему оно важно для продвижения сайтов и какие преимущества дает использование методов поисковой оптимизации.' },
+  { id: 2, title: 'Как работают поисковые системы?', description: 'Как поисковые системы собирают, анализируют и показывают информацию пользователям. Ключевые этапы работы поисковых систем и их значимость для SEO.' },
+  { id: 3, title: 'Техническое SEO', description: 'Важность технического SEO для улучшения видимости сайта в поисковых системах.' },
+  { id: 4, title: 'Внутренняя оптимизация сайта', description: 'Важные аспекты внутренней оптимизации сайта, которые помогают улучшить видимость страниц в поисковых системах и сделать сайт более удобным для пользователей.' },
+  { id: 5, title: 'Работа с контентом', description: 'Как правильно работать с контентом на странице, включая оптимизацию текста для SEO.' },
+  { id: 6, title: 'Внутренние ссылки', description: 'Внутренние ссылки, их значением для SEO и как правильно их использовать на сайте для улучшения навигации и поисковой оптимизации.' },
+  { id: 7, title: 'Локальное SEO', description: 'Локальное SEO помогает вашему бизнесу отображаться в результатах поиска, когда пользователи ищут товары или услуги в конкретных географических местах.' },
+  { id: 8, title: 'SEO-аналитика', description: 'SEO-аналитика играет важную роль в оценке эффективности ваших SEO-усилий и позволяет отслеживать источники трафика, а также поведение пользователей на вашем сайте.' },
+  { id: 9, title: 'Советы и тренды SEO', description: 'Веб-среда постоянно меняется, и вместе с ней развиваются новые методы SEO-оптимизации. Чтобы оставаться конкурентоспособным в поисковых системах, важно следить за современными тенденциями и рекомендациями.' },
+];
+
+export default function HomePage() {
+  const [lessons, setLessons] = useState<Lesson[]>([]);
+
+  useEffect(() => {
+    const updatedLessons = lessonsData.map((lesson) => {
+      const cookieProgress = Cookies.get(`lesson_${lesson.id}_progress`) || '0';
+      return { ...lesson, progress: parseInt(cookieProgress) };
+    });
+    setLessons(updatedLessons);
+  }, []);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+    <>
+      <header className="bg-green-500 text-white py-5 shadow-lg mb-5">
+        <h1 className="text-center text-2xl font-bold">
+          Основы SEO
+        </h1>
+      </header>
+      <main className="bg-gray-100 py-5">
+        <div className="container mx-auto w-[90%] max-w-[1200px]">
+          <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {lessons.map((lesson) => (
+              <Link
+                href={`/lesson/${lesson.id}`}
+                key={lesson.id}
+                className="text-black block p-4 border rounded-lg transition-transform hover:-translate-y-1 hover:shadow-2xl"
+              >
+                <div className="text-gray-800 text-xl font-bold mb-2.5">Урок {lesson.id}: {lesson.title}</div>
+                <div className="text-gray-600 text-base">{lesson.description}</div>
+                {lesson.id < 9 && (
+                  <div className="relative w-full h-6 bg-gray-200 rounded">
+                    <div
+                      className="absolute top-0 left-0 h-full bg-green-500 rounded"
+                      style={{ width: `${lesson.progress}%` }}
+                    ></div>
+                    <div className="absolute inset-0 text-center text-sm leading-6">
+                      {lesson.progress}%
+                    </div>
+                  </div>
+                )}
+              </Link>
+            ))}
+          </section>
+          </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </>
   );
 }
